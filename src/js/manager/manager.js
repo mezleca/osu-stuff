482dbdaf1de85c6f7046d713fa37d00de75b09d6
mezleca <mtksenpai@gmail.com>
2024-09-13 16:35:18 -0300

    revert changes to manager
    current method sucks for search and long collections
    so i gonna rework this later

--- src/js/manager/manager.js
+++ src/js/manager/manager.js
@@ -10,7 +10,7 @@ import { download_map } from "../utils/download_maps.js";
 let current_name = "";
 
 export const collections = new Map();
-const search_index = new Map();
+const placeholder_img = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 const collection_list = document.querySelector(".collection-list");
 const main_content = document.querySelector(".main-content");
@@ -60,7 +60,7 @@ const remove_beatmap = (hash) => {
     document.getElementById(hash).remove();
 };
 
-const render_tab = async (tab, beatmaps) => {
+const render_tab = (tab, beatmaps) => {
 
     const fragment = document.createDocumentFragment();
 
@@ -82,173 +82,94 @@ const render_tab = async (tab, beatmaps) => {
         </div>
     `;
 
-    const render = (start, end) => {
-
-        return new Promise(resolve => {
-
-            requestAnimationFrame(() => {
-
-                for (let i = start; i < end && i < beatmaps.length; i++) {
-
-                    const beatmap = beatmaps[i];
-                    const item = template.content.cloneNode(true).firstElementChild;
-
-                    map_item(item, beatmap);
-                    fragment.appendChild(item);
-                }
-
-                resolve();
-            });
-        });
-    };
-
-    for (let i = 0; i < beatmaps.length; i += 15) {
-
-        await render(i, i + 15);
-
-        if (i === 0) {
-            tab.appendChild(fragment.cloneNode(true));
-        }
-    }
-
-    tab.appendChild(fragment);
-    requestAnimationFrame(lazyLoad);
-};
-
-const render_2 = async (tab, beatmaps, size = 15) => {
-
-    const fragment = document.createDocumentFragment();
-    const template = document.createElement('template');
-
-    template.innerHTML = `
-        <div class="mini-container">
-            <img class="bg-image">
-            <div class="content">
-                <div class="small-image-container">
-                    <img class="small-image lazy" loading="lazy">
-                </div>
-                <div class="text-container">
-                    <p class="title"></p>
-                    <p class="subtitle"></p>
-                </div>
-                <button class="download-button"><i class="bi bi-download"></i></button>
-                <button class="remove-btn"><i class="bi bi-trash-fill"></i></button>
-            </div>
-        </div>
-    `;
-
-    const render = (start, end) => {
-
-        return new Promise(resolve => {
-
-            requestAnimationFrame(() => {
-
-                for (let i = start; i < end && i < beatmaps.length; i++) {
-
-                    const beatmap = beatmaps[i];
-                    const item = template.content.cloneNode(true).firstElementChild;
-
-                    map_item(item, beatmap);
-                    fragment.appendChild(item);
-                }
-                resolve();
-            });
-        });
-    };
-
-    for (let i = 0; i < beatmaps.length; i += size) {
+    beatmaps.forEach((beatmap) => {
         
-        await render(i, i + size);
+        const map_item = template.content.cloneNode(true).firstElementChild;
+        const has_beatmap = Boolean(beatmap.artist_name);
+        const has_bg = Boolean(beatmap.bg);
 
-        if (i === 0) {
-            tab.appendChild(fragment.cloneNode(true));
+        map_item.dataset.title = has_beatmap ? `${beatmap.artist_name} - ${beatmap.song_title} [${beatmap.difficulty}]`.toLowerCase() : "Unknown (not downloaded)".toLowerCase();
+        map_item.dataset.mapper = beatmap.creator_name ? beatmap.creator_name.toLowerCase() : "Unknown";
+        map_item.dataset.tags = beatmap.tags ? beatmap.tags.toLowerCase() : "";
+        map_item.dataset.artist = beatmap.artist_name ? beatmap.artist_name.toLowerCase() : "";
+
+        map_item.id = beatmap.md5;
+
+        const title = map_item.querySelector('.title');
+        const subtitle = map_item.querySelector('.subtitle');
+        const small_bg = map_item.querySelector('.small-image');
+        const download_btn = map_item.querySelector('.download-button');
+        const remove_btn = map_item.querySelector('.remove-btn');
+
+        title.textContent = map_item.dataset.title;
+        subtitle.textContent = has_beatmap ? `mapped by ${beatmap.creator_name}` : "mapped by Unknown";
+
+        const small_img_path = path.resolve(core.config.get("osu_path"), `Data`, `bt`, `${beatmap.beatmap_id}l.jpg`);
+        const beatmap_image_url = `https://assets.ppy.sh/beatmaps/${beatmap.beatmap_id}/covers/list@2x.jpg`;
+
+        small_bg.dataset.src = has_bg ? beatmap.bg : (fs.existsSync(small_img_path) ? small_img_path : beatmap_image_url);
+        remove_btn.id = `bn_${beatmap.beatmap_id}`;
+        
+        if (has_beatmap) {
+            small_bg.addEventListener("click", () => {
+                const url = beatmap.url || `https://osu.ppy.sh/b/${beatmap.difficulty_id}`;
+                shell.openExternal(url);
+            });
         }
-    }
+
+        remove_btn.addEventListener("click", () => {
+            console.log("removing beatmap", beatmap.song_title, current_name, beatmap.md5);
+            remove_beatmap(beatmap.md5);
+        });
+
+        if (!has_beatmap) {
+
+            download_btn.addEventListener("click", async () => {
+
+                add_alert("Searching beatmap...");
+
+                const beatmap_data = await download_map(beatmap.md5);
+
+                if (!beatmap_data) {
+                    add_alert("Beatmap not found :c", { type: "alert" });
+                    return;
+                }
+
+                beatmap.artist_name = beatmap_data.beatmapset.artist;
+                beatmap.song_title = beatmap_data.beatmapset.title;
+                beatmap.difficulty = beatmap_data.version;
+                beatmap.md5 = beatmap_data.checksum;
+                beatmap.creator_name = beatmap_data.beatmapset.creator;
+                beatmap.difficulty_id = beatmap_data.beatmapset_id;
+
+                beatmap.url = beatmap_data.url;
+                beatmap.bg = beatmap_data.beatmapset.covers.list;
+    
+                const title_text = `${beatmap.artist_name} - ${beatmap.song_title} [${beatmap.difficulty}]`
+                const mapper_text = `mapped by ${beatmap.creator_name}`;
+
+                small_bg.src = beatmap.bg;
+        
+                title.innerText = title_text;
+                subtitle.innerText = mapper_text;
+
+                download_btn.remove();
+
+                small_bg.addEventListener("click", () => {
+                    shell.openExternal(beatmap_data.url);
+                });
+            });
+        } else {
+            download_btn.remove();
+        }
+
+        fragment.appendChild(map_item);
+    });
 
     tab.appendChild(fragment);
-    requestAnimationFrame(lazyLoad);
-};
 
-const map_item = (map_item, beatmap) => {
-
-    const has_beatmap = Boolean(beatmap.artist_name);
-    const has_bg = Boolean(beatmap.bg);
-
-    map_item.dataset.title = has_beatmap ? `${beatmap.artist_name} - ${beatmap.song_title} [${beatmap.difficulty}]` : "Unknown (not downloaded)";
-    map_item.dataset.mapper = beatmap.creator_name || "Unknown";
-    map_item.id = beatmap.md5;
-
-    const title = map_item.querySelector('.title');
-    const subtitle = map_item.querySelector('.subtitle');
-    const small_bg = map_item.querySelector('.small-image');
-    const download_btn = map_item.querySelector('.download-button');
-    const remove_btn = map_item.querySelector('.remove-btn');
-
-    title.textContent = map_item.dataset.title;
-    subtitle.textContent = has_beatmap ? `mapped by ${beatmap.creator_name}` : "mapped by Unknown";
-
-    const small_img_path = path.resolve(core.config.get("osu_path"), `Data`, `bt`, `${beatmap.beatmap_id}l.jpg`);
-    const beatmap_image_url = `https://assets.ppy.sh/beatmaps/${beatmap.beatmap_id}/covers/list@2x.jpg`;
-
-    small_bg.dataset.src = has_bg ? beatmap.bg : (fs.existsSync(small_img_path) ? small_img_path : beatmap_image_url);
-    remove_btn.id = `bn_${beatmap.beatmap_id}`;
-
-    if (has_beatmap) {
-        small_bg.addEventListener("click", () => {
-            const url = beatmap.url || `https://osu.ppy.sh/b/${beatmap.difficulty_id}`;
-            shell.openExternal(url);
-        });
-    }
-
-    remove_btn.addEventListener("click", () => {
-        console.log("removing beatmap", beatmap.song_title, current_name, beatmap.md5);
-        remove_beatmap(beatmap.md5);
-    });
-
-    if (!has_beatmap) {
-
-        download_btn.addEventListener("click", async () => {
-
-            const beatmap_data = await download_map(beatmap.md5);
-
-            if (!beatmap_data) {
-                add_alert("beatmap not found :c", { type: "alert" });
-                return;
-            }
-
-            update_ui(beatmap, beatmap_data, map_item);
-            add_alert("finished downloading beatmap");
-        });
-    } else {
-        download_btn.remove();
-    }
-};
-
-const update_ui = (beatmap, beatmap_data, map_item) => {
-
-    Object.assign(beatmap, {
-        artist_name: beatmap_data.beatmapset.artist,
-        song_title: beatmap_data.beatmapset.title,
-        difficulty: beatmap_data.version,
-        md5: beatmap_data.checksum,
-        creator_name: beatmap_data.beatmapset.creator,
-        difficulty_id: beatmap_data.beatmapset_id,
-        url: beatmap_data.url,
-        bg: beatmap_data.beatmapset.covers.list
-    });
-
-    const title = map_item.querySelector('.title');
-    const subtitle = map_item.querySelector('.subtitle');
-    const small_bg = map_item.querySelector('.small-image');
-    const download_btn = map_item.querySelector('.download-button');
-
-    title.innerText = `${beatmap.artist_name} - ${beatmap.song_title} [${beatmap.difficulty}]`;
-    subtitle.innerText = `mapped by ${beatmap.creator_name}`;
-    small_bg.src = beatmap.bg;
-    download_btn.remove();
-
-    small_bg.addEventListener("click", () => {
-        shell.openExternal(beatmap_data.url);
+    requestAnimationFrame(() => {
+        lazyLoad();
     });
 };
 
@@ -275,7 +196,7 @@ const create_container = (name) => {
     return container;
 };
 
-btn_rename.addEventListener("click", async () => {
+btn_rename.addEventListener("click", () => {
 
     const input = document.getElementById("collection_input_name");
 
@@ -284,7 +205,7 @@ btn_rename.addEventListener("click", async () => {
         return;
     }
 
-    // value didn't change
+    // value dit not changed
     if (input.value == current_name) {
         return;
     }
@@ -307,76 +228,40 @@ btn_rename.addEventListener("click", async () => {
 
     // remove the current container and re-render the new tab
     document.querySelector(".collection-container").remove();
-    await render_tab(create_container(input.value), collections.get(input.value));
-    build_search_index();
+    render_tab(create_container(input.value), collections.get(input.value));
 
     current_name = input.value;
 });
 
-function build_search_index() {
-    
+search_box.addEventListener("input", () => {
+
+    const name = search_box.value.toLowerCase();
     const elements = document.querySelectorAll(".mini-container");
 
-    elements.forEach((element) => {
-        const searchString = (element.dataset.title + " " + element.dataset.mapper).toLowerCase();
-        search_index.set(element, searchString);
-    });
-}
-
-function debounce(func, delay) {
-
-    let timeout;
-
-    return function (...args) {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, args), delay);
+    if (!name) {
+        elements.forEach((e) => e.classList.remove("disabled"));
     };
-}
 
-// TODO: fix really bad lag after searching something and clearing the query
-const search = debounce(async (query) => {
+    elements.forEach((e) => {
 
-    const elements = Array.from(search_index.keys());
-    const beatmaps = collections.get(current_name);
-    const collection_container = document.querySelector(".collection-container");
-
-    if (!collection_container) {
-        console.log("[ERROR] collection container not found");
-        return;
-    }
-
-    if (query == "") {
-        elements.forEach((element) => element.remove());
-        render_2(collection_container, beatmaps);
-        build_search_index();   
-        return;
-    }
-
-    const filtered = beatmaps.filter((beatmap) => {
-
-        const title = beatmap.song_title;
-        const artist = beatmap.artist_name;
-        const mapper = beatmap.creator_name;
-        const tags = beatmap.tags;
-
-        if (!title) {
+        if (e.dataset.title.includes(name)) {
             return;
         }
 
-        return title.toLowerCase().includes(query.toLowerCase())  || 
-               artist.toLowerCase().includes(query.toLowerCase()) || 
-               mapper.toLowerCase().includes(query.toLowerCase()) || 
-               tags.toLowerCase().includes(query.toLowerCase());
+        if (e.dataset.mapper.includes(name)) {
+            return;
+        }
+    
+        if (e.dataset.artist.includes(name)) {
+            return;
+        }
+
+        if (e.dataset.tags.includes(name)) {
+            return;
+        }
+
+        e.classList.add("disabled");
     });
-
-    elements.forEach((element) => element.remove());
-
-    await render_2(collection_container, filtered);
-    build_search_index();
-}, 1000); 
-
-search_box.addEventListener("input", (event) => {
-    search(event.target.value);
 });
   
 btn_add.addEventListener("click", async () => {
@@ -415,8 +300,7 @@ btn_add.addEventListener("click", async () => {
         document.querySelector(".collection-container").remove();
     }
     
-    await render_tab(create_container(collection.name), collections.get(collection.name));
-    build_search_index();
+    render_tab(create_container(collection.name), collections.get(collection.name));
 });
 
 btn_update.addEventListener("click", async () => {
@@ -504,7 +388,7 @@ function lazyLoad() {
     images.forEach(img => imageObserver.observe(img));
 };
 
-const setup_manager = async () => {
+const setup_manager = () => {
 
     collection_list.innerHTML = "";
 
@@ -512,12 +396,12 @@ const setup_manager = async () => {
     const template = document.createElement('template');
     template.innerHTML = '<div class="collection-item"></div>';
 
-    collections.forEach(async (v, k) => {
+    collections.forEach((v, k) => {
 
         const newCollection = template.content.cloneNode(true).firstElementChild;
         newCollection.textContent = k;
 
-        newCollection.addEventListener("click", async () => {
+        newCollection.addEventListener("click", () => {
 
             const collection_text = document.getElementById("collection_text");
 
@@ -528,8 +412,7 @@ const setup_manager = async () => {
             const all_collections_text = Array.from(collection_list.children);
 
             remove_container();
-            await render_tab(create_container(k), v);
-            build_search_index();
+            render_tab(create_container(k), v);
             change_input_value(k);
 
             all_collections_text.forEach(e => e.classList.remove("selected"));
@@ -596,7 +479,7 @@ export const initialize = async (options) => {
         container.remove();
     }
 
-    await setup_manager();
+    setup_manager();
 
     console.log("manager has been initialized");
 };
