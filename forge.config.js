ab56bf6d538ce5a2dac87faa8a02cdf2086e77c2
mezleca <mtksenpai@gmail.com>
2025-04-07 13:10:21 -0300

    fix asar file including realm prebuild files that are over 900MBS!! and some other changes to the "build" script

--- forge.config.js
+++ forge.config.js
@@ -1,43 +1,66 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
-  packagerConfig: {
-    asar: true,
-    icon: './build/icons/win/icon',
-    executableName: 'osu-stuff',
-    appBundleId: 'com.osustuff.app'
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {
-        name: 'osu-stuff',
-        setupIcon: './build/icons/win/icon.ico',
-        setupExe: `osu-stuff-${process.env.npm_package_version}.exe`,
-        noMsi: false,
-        requireAdministrator: true
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {
-        options: {
-          icon: './build/icons/png/256x256.png',
-          categories: ['Utility']
-        }
-      },
-    },
-    {
-      name: "@reforged/maker-appimage",
-      config: {
-        options: {
-          categories: ["Network"],
-          icon: './build/icons/png/256x256.png',
-        }
-      }
-    }
-  ],
+	packagerConfig: {
+		asar: true,
+		icon: path.resolve(__dirname, "./build/icons/win/icon"),
+		executableName: "osu-stuff",
+		appBundleId: "com.osustuff.app",
+		ignore: [
+			"\\.git(ignore|modules)",
+			"node_modules/\\.bin",
+			"\\.vscode",
+			"test",
+			"tests",
+			"node_modules/realm/prebuilds/android-*",
+			"node_modules/realm/prebuilds/apple-*",
+		].filter(Boolean),
+	},
+	rebuildConfig: {},
+	makers: [
+		{
+			name: "@electron-forge/maker-squirrel",
+			config: {
+				name: "osu-stuff",
+				setupIcon: path.resolve(
+					__dirname,
+					"./build/icons/win/icon.ico",
+				),
+				setupExe: `osu-stuff-${process.env.npm_package_version}.exe`,
+				noMsi: false,
+				requireAdministrator: true,
+			},
+		},
+		{
+			name: "@electron-forge/maker-zip",
+			platforms: ["darwin"],
+		},
+		{
+			name: "@electron-forge/maker-deb",
+			config: {
+				options: {
+					icon: path.resolve(
+						__dirname,
+						"./build/icons/png/256x256.png",
+					),
+					categories: ["Utility"],
+				},
+			},
+		},
+		{
+			name: "@reforged/maker-appimage",
+			config: {
+				options: {
+					categories: ["Network"],
+					icon: path.resolve(
+						__dirname,
+						"./build/icons/png/256x256.png",
+					),
+				},
+			},
+		},
+	],
 };
