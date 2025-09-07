9f271b1b30ca121ae55801a1f788ce5abb088561
mezleca <mtksenpai@gmail.com>
2025-09-07 00:38:12 -0300

    feat(beatmaps): beatmap builder (.osz)

---
+++ src/main/beatmaps/builder.js
@@ -0,0 +1,163 @@
+import JSZip from "jszip";
+import fs from "fs";
+import path from "path";
+
+const DEFAULT_AUDIO_NAME = "audio.mp3";
+
+// @TODO: background / storyboard support
+class LegacyBeatmapFile {
+    constructor() {
+        this.properties = new Map();
+        this.version = "osu file format v14\n";
+        this.section_data = new Map([
+            // @TODO: idk if this is broken, tested with a .opus file and dint work (maybe .opus issue? idk)
+            [
+                "General",
+                [
+                    { key: "AudioFilename", required: true },
+                    { key: "SampleSet", value: "None" },
+                    { key: "AudioLeadIn", value: 0 },
+                    { key: "PreviewTime", value: -1 },
+                    { key: "Countdown", value: 0 },
+                    { key: "StackLeniency", value: 0.7 },
+                    { key: "Mode", value: 0 },
+                    { key: "LetterboxInBreaks", value: 0 },
+                    { key: "WidescreenStoryboard", value: 0 }
+                ]
+            ],
+            [
+                "Editor",
+                [
+                    { key: "DistanceSpacing", value: 2 },
+                    { key: "BeatDivisor", value: 4 },
+                    { key: "GridSize", value: 1 },
+                    { key: "TimelineZoom", value: 1 }
+                ]
+            ],
+            [
+                "Metadata",
+                [
+                    { key: "Title", required: true },
+                    { key: "TitleUnicode", value: "" },
+                    { key: "Artist", required: true },
+                    { key: "ArtistUnicode", value: "" },
+                    { key: "Creator", value: "osu-stuff" },
+                    { key: "Version", value: "hello, world!" },
+                    { key: "Source", value: "" },
+                    { key: "Tags", value: "" },
+                    { key: "BeatmapID", value: 0 },
+                    { key: "BeatmapSetID", value: -1 }
+                ]
+            ],
+            [
+                "Difficulty",
+                [
+                    { key: "HPDrainRate", value: 5 },
+                    { key: "CircleSize", value: 5 },
+                    { key: "OverallDifficulty", value: 5 },
+                    { key: "ApproachRate", value: 5 },
+                    { key: "SliderMultiplier", value: 1.4 },
+                    { key: "SliderTickRate", value: 1 }
+                ]
+            ],
+            ["Events", []],
+            ["HitObject", []]
+        ]);
+    }
+
+    set(key, value) {
+        this.properties.set(key, value);
+    }
+
+    get(key) {
+        return this.properties.get(key);
+    }
+
+    has(key) {
+        return this.properties.has(key);
+    }
+}
+
+class BeatmapBuilder {
+    constructor() {}
+
+    create() {
+        return new LegacyBeatmapFile();
+    }
+
+    read() {
+        throw new Error("read(): not implemented yet");
+    }
+
+    /** @param {LegacyBeatmapFile} file */
+    write(file) {
+        if ((!file) instanceof LegacyBeatmapFile) {
+            throw new Error("NOOOOOOOOOO");
+        }
+
+        // initialize buffer with version
+        const buffer = [file.version];
+
+        for (const [section, data] of file.section_data) {
+            // write new section
+            buffer.push(`[${section}]`);
+
+            // add section properties
+            for (const property of data) {
+                let value = "";
+                const has_custom_value = file.has(property.key);
+
+                // throws if we dont have a required key
+                if (property.required && !has_custom_value) {
+                    throw new Error(`builder: missing ${property.key}`);
+                }
+
+                // mhm
+                if ((property.key == "ArtistUnicode" || property.key == "TitleUnicode") && !has_custom_value) {
+                    value = file.get(property.key.split("U")[0]) ?? "";
+                }
+
+                // fallback to default value if available
+                if (!value) {
+                    value = has_custom_value ? file.get(property.key) : property.value;
+                }
+
+                buffer.push(`${property.key}:${value}`);
+            }
+
+            buffer.push(""); // add blank line between each section
+        }
+
+        return buffer.join("\n");
+    }
+
+    /** @param {LegacyBeatmapFile} file */
+    async zip(file) {
+        const buffer = this.write(file);
+        const zip = new JSZip();
+
+        const file_location = file.get("AudioLocation");
+        const file_name = file.get("Title");
+
+        // ensure audio file exists
+        if (!fs.existsSync(file_location)) {
+            console.log("builder: failed to find audio file");
+            return false;
+        }
+
+        // add audio file
+        zip.file(DEFAULT_AUDIO_NAME, fs.readFileSync(file_location));
+
+        // add .osu file
+        zip.file(`${file_name}.osu`, buffer);
+
+        const zipped = await zip.generateAsync({
+            type: "nodebuffer",
+            compression: "DEFLATE"
+        });
+
+        return zipped;
+    }
+}
+
+export const beatmap_builder = new BeatmapBuilder();
